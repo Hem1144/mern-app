286c86b52b9125abaf46ed7369754d7f77f22d54
Hem1144 <hemlaldulal.986@gmail.com>
2024-07-19 17:36:36 +0545

    feat: Managed protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import TaskList from "./components/task/TaskList";
 import TaskForm from "./components/task/TaskForm";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/PrivateRoute";
 import "./styles/App.css";
 
 const App = () => {
@@ -15,11 +15,15 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route element={<PrivateRoute />}> */}
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-        <Route path="/tasks/:id" element={<TaskForm />} />
-        {/* </Route> */}
+        <Route path="/" element=<TaskList /> />
+        <Route
+          path="/tasks/new"
+          element={<ProtectedRoute element={<TaskForm />} />}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<ProtectedRoute element={<TaskForm />} />}
+        />
       </Routes>
     </Router>
   );
--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const PrivateRoute = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+const ProtectedRoute = ({ element }) => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return isAuthenticated ? element : <Navigate to="/login" />;
 };
 
-export default PrivateRoute;
+export default ProtectedRoute;
